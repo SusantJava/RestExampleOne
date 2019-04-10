cfd1314d298c48102660fc8ef852b1e7a74e1084
sis <sis@192.168.0.2>
2019-04-11 00:51:27 +0530

    Initial Commit

---
+++ TestTwo/src/com/app/Test.java
@@ -0,0 +1,12 @@
+package com.app;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+@Path("/show")
+public class Test {
+	@POST
+	public void showA() {
+		System.out.println("Happy path");
+	}
+
+}
